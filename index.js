3bfbfe5ac1ca7514be9b683ef9b0252a9f198e5e
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-02-06 22:35:52 +0545

    Added book route index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
 const express = require("express");
-const indexRouter = require("./routes");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
+const indexRouter = require("./routes");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
+
+mongoose.connect("mongodb://localhost:27017/book-mgmt").then(() => {
+  console.log("Database connected");
+});
 
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
 app.use("/", indexRouter);
 
 app.use((err, req, res, next) => {
--- modules/books/book.route.js
+++ modules/books/book.route.js
@@ -1,7 +1,52 @@
-const bookRouter = require("express").Router();
+const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  res.json({ msg: "hello from book route" });
+  try {
+    res.json({ msg: "hello from book get route" });
+  } catch (e) {
+    next(e);
+  }
 });
 
-module.exports = bookRouter;
+router.post("/", (req, res, next) => {
+  try {
+    const data = req.body;
+    console.log({ data });
+    res.json({ msg: "Hello from book post route" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    console.log({ id, data });
+    res.json({ msg: "Hello from book put route" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.patch("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    console.log({ id, data });
+    res.json({ msg: "Hello from book patch route" });
+  } catch (e) {
+    next(e);
+  }
+});
+router.delete("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log({ id });
+    res.json({ msg: "Hello from book delete route" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
